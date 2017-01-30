37fac597a9933e0242b04b77efe5b08347cc7f3b
kadota <kadota@fenrir.co.jp>
2017-01-30 13:26:05 +0900

    set var- and fix-font to Go fonts if exist

--- acmeedit.bash
+++ acmeedit.bash
@@ -6,4 +6,14 @@ then
 	PATH=$PATH:$PLAN9/bin
 fi
 
-exec acme "$@"
+opts=()
+if 9p ls font | grep -q GoRegular
+then
+	opts=("${opts[@]}" -f /mnt/font/GoRegular/14a/font)
+fi
+if 9p ls font | grep -q GoMono
+then
+	opts=("${opts[@]}" -F /mnt/font/GoMono/14a/font)
+fi
+
+acme "${opts[@]}" "$@"
